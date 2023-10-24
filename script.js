2bc0c4e4aada1d05deabe53e760a866d79dff4c6
michaelparkernorton <72527646+michaelparkernorton@users.noreply.github.com>
2023-10-24 17:48:33 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+color.onclick = function() {
+    this.style.backgroundColor = 'blue';
+    this.style.color = 'white';
+};
+
+growing.onclick = function () {
+  this.style.fontSize = "2px";
+  this.style.color = "green";
+};
+
+stripe.onclick = function() {
+    stripe.classList.add('animate');
+};
+digit2.onclick = function() {
+    stripe2.classList.add('animate2');
+}
